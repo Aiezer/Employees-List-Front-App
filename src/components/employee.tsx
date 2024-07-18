5d10a0ffc9bf7fa5b1e9b6987446def598a60402
AiezerSantos <aiezer.santos01@gmail.com>
2024-07-17 23:37:49 -0300

    Feat: Criar componente Employee
    Criado o componente funcional `Employee` em React para exibir informações de funcionários, incluindo nome, cargo, data de admissão, telefone e imagem. O componente permite expandir e recolher detalhes adicionais ao clicar em um ícone de chevron.
    
    - Utiliza `useState` do React para gerenciar o estado de expansão.
    - Exibe o nome do funcionário centralizado entre a imagem e o botão de expandir/recolher.
    - Usa ícones `BsChevronDown` e `BsChevronUp` do `react-icons/bs` para funcionalidade de expansão.
    
    O componente possui um layout estruturado com classes do Tailwind CSS para estilização.
    
    Alterações:
    - Criado src/components/Employee.tsx
    - Adicionada interface EmployeeProps para segurança de tipos.

--- src/components/employee.tsx
+++ src/components/employee.tsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import EmployeeProps from '../interfaces/EmployeeProps';
-
+import { useState } from 'react';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 
 const Employee: React.FC<EmployeeProps> = ({ id, name, job, admissionDate, phone, image }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <div>
-      {name}
-    </div>
-  )
+    <div className={`employeeId=${id} p-5 border-b bg-white`}>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img src={image} alt={name} className="w-10 h-10 rounded-full" />
+        </div>
+        <div className="ml-4">
+          <div className="text-lg font-medium">{name}</div>
+        </div>
+        <button onClick={() => setExpanded(!expanded)} >
+          {<button onClick={() => setExpanded(!expanded)}>
+            {expanded ? (
+              <>
+                <BsChevronUp className="h-6 w-6 flex items-center justify-center" />
+              </>
+            ) : (
+              <>
+                <BsChevronDown className="h-6 w-6 flex items-center justify-center" />
+              </>
+            )}
+          </button>}
+        </button>
+      </div>
+      {
+        expanded && (
+          <div className="mt-2 text-sm text-gray-500">
+            <div>Cargo: {job}</div>
+            <div>Data de admissão: {admissionDate}</div>
+            <div>Telefone: {phone}</div>
+          </div>
+        )
+      }
+    </div >
+  );
 }
 
 export default Employee;
